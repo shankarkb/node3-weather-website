6b64f348c731e32518fecb3ea4fe99c57f232372
bhavanikbs <kbshankar715@gmail.com>
2020-05-13 23:36:45 +0530

    Changing the properties for script.js to support HTML

--- public/js/script.js
+++ public/js/script.js
@@ -15,7 +15,7 @@ weatherForm.addEventListener('submit', (e) => {
                 messageOne.textContent=data.error
             }else{
                 messageOne.textContent=data.location
-                messageTwo.textContent=data.forecast
+                messageTwo.innerHTML=data.forecast
             }
         })
     })
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
                 callback('Sorry, these is no latitude/longitude!', undefined)
             } else {
                 // const data = JSON.parse(response.body)
-                callback(undefined, "<img src='" + body.current.weather_icons[0]+"' /> <br>" + body.current.weather_descriptions +'. It is currently ' + response.body.current.temperature + ' degrees out. There is a ' + response.body.current.precip + '% chance of rain'+'<br> It feels like ' + response.body.current.feelslike + ' out.<br>Humidity: ' + response.body.current.humidity + '%')
+                callback(undefined, "<img src='" + body.current.weather_icons[0]+"' /> <br>" + body.current.weather_descriptions +'. It is currently ' + response.body.current.temperature + ' degrees out. There is a ' + response.body.current.precip + '% chance of rain.'+'<br> It feels like ' + response.body.current.feelslike + ' out.<br>Humidity: ' + response.body.current.humidity + '%')
             }
         
     })   
